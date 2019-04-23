2c34ab59f4bddb288a43f8eb6380316afcfb450d
Will <william8ga+taobaotools@gmail.com>
2019-04-23 14:09:17 +1000

    Add JS

---
+++ js/app.js
@@ -0,0 +1,163 @@
+/**
+ * Helper class that does all the calculations and converting. All UI element
+ * interactions are handled in 'preparer.js'
+ */
+const ITEM_ID = 'id=';
+const ITEM = 'item/';
+const SHOP_ID = 'shop_id=';
+const TAOBAO_URL = 'https://item.taobao.com/item.htm?id=';
+const TMALL_URL = 'https://detail.tmall.com/item.htm?id=';
+const SHOP_URL = 'https://shop{}.taobao.com';
+const M_INTL = 'm.intl.taobao.com';
+const H5 = 'h5.m.taobao.com';
+const WORLD = 'world.taobao.com';
+const SHOP_M = 'shop.m.taobao.com';
+const SEARCH_QUERY = '/search.htm?search=y';
+
+/**
+ * Given an array of URLs, separate them into valid and invalid ones, convert them and return
+ */
+function convertURLs(urls, removeWhitespace, extractSuperbuy) {
+    console.log('Remove whitespace: ' + removeWhitespace + ', extract Superbuy links: ' + extractSuperbuy);
+    // Initialise our empty arrays
+    var validLinks = [];
+    var invalidLinks = [];
+
+    // Go through each element of the parameter array and convert as required
+    urls.forEach(function (url) {
+        console.log('Handling ' + url);
+        if (removeWhitespace) {
+            url = url.replace(/\s/g, "");
+        }
+        if (extractSuperbuy && contains(url, 'superbuy.com')) {
+            url_obj = new URL(url);
+            url_params = url_obj.searchParams
+
+            if (url_params.has('url')) {
+                url = url_params.get('url');
+                url = decodeURIComponent(url);
+                console.log('URL extracted from Superbuy link: ' + url);
+            }
+	    }
+        if (isTaobaoURL(url))
+            validLinks.push(convertURL(url));
+        else
+            invalidLinks.push(url);
+    });
+
+    // Return object containing our valid links and invalid links array
+    return {
+        valid: validLinks,
+        invalid: invalidLinks
+    };
+}
+
+
+/**
+ * Given a valid, Taobao url we convert it into Chinese mainland form
+ */
+function convertURL(str) {
+    // Handle tmall world link
+    if (contains(str, 'tmall.com'))
+        return TMALL_URL + getID(str, ITEM_ID);
+
+    // Handle Taobao links
+    if (contains(str, M_INTL) || contains(str, H5)) {
+        // Normal international or H5 app link
+        return buildTaobaoURL(str, ITEM_ID, false)
+    } else if (contains(str, WORLD)) {
+        // world.taobao.com link
+        if (contains(str, 'item')) {
+            // World item
+            return buildTaobaoURL(str, ITEM, false);
+        } else {
+            // World store
+            var intermediate = str.replace('world.taobao.com', 'taobao.com');
+            return cleanTaobaoStore(intermediate);
+        }
+    } else if (contains(str, SHOP_M)) {
+        // Mobile shop with shop ID
+        return buildTaobaoURL(str, SHOP_ID, true);
+    } else if (!contains(str, 'item')) {
+        // Clean the store link, remove any redundant information, and conevrt from mobile if relevant
+        // str.replace('m.taobao.com', 'taobao.com'); // MOBILE STORE
+        return cleanTaobaoStore(str);
+    } else {
+        // Already valid Taobao URL, canonacalise it
+        return buildTaobaoURL(str, 'id=', false);
+    }
+}
+
+
+/**
+ * Given a string URL and a query we want to match, get the ID which is numeric only
+ */
+function getID(str, match) {
+    var start = str.indexOf(match) + match.length;
+    var end = str.length;
+    for (var i = start; i < str.length; i++) {
+        if (!isDigit(str.charAt(i))) {
+            end = i;
+            break;
+        }
+    }
+    return str.substring(start, end);
+}
+
+
+/**
+ * Given a string, and query we want to match, we find, replace and return
+ */
+function buildTaobaoURL(str, match, isShop) {
+    if (isShop) {
+        var shopID = getID(str, match);
+        return SHOP_URL.replace('{}', shopID);
+    } else {
+        var itemID = getID(str, match);
+        return TAOBAO_URL + itemID;
+    }
+}
+
+/**
+ * Clean a Taobao store search
+ */
+function cleanTaobaoStore(str) {
+    // Check if there are additional useless information variables
+    var end = str.indexOf('taobao.com/');
+    if (end == -1) {
+        return str;
+    } else {
+        end += 10;
+        if (contains(str, 'search.htm'))
+            return str.substring(0, end) + SEARCH_QUERY;
+        else
+            return str.substring(0, end);
+    }
+}
+
+
+/**
+ * Given a string, we check and return if it is a valid Taobao URL or not
+ */
+function isTaobaoURL(str) {
+    // Define our Regex pattern and test the string
+    var urlPattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+    return urlPattern.test(str) && (str.indexOf('taobao.com') != -1 || str.indexOf('tmall.com') != -1);
+}
+
+
+/**
+ * Given a string and a query parameter, we check if the string contains that query
+ */
+function contains(str, query) {
+    return str.indexOf(query) != -1;
+}
+
+
+/**
+ * Given a character, check whether it is a digit or not by converting to ASCII code
+ */
+function isDigit(char) {
+    char = char.charCodeAt(0);
+    return char >= 48 && char <= 57;
+}
---
+++ js/preparer.js
@@ -0,0 +1,162 @@
+const CONFIRM = 'WARNING! Are you sure you want to open these links? Since there are {} links, you ' +
+    'may temporarily experience some lag in your web browser';
+
+// Global variables for storing the links, not a very good idea...
+var validLinks;
+var invalidLinks;
+
+var optionsVisible = false;
+
+function toggleOptionVisibility(showOptions) {
+    if (showOptions) {
+        setDisplay('options', true); 
+        setDisplay('showOptions', false); 
+        setDisplay('hideOptions', true);
+        optionsVisible = true;
+    } else {
+        setDisplay('options', false); 
+        setDisplay('showOptions', true); 
+        setDisplay('hideOptions', false);
+        optionsVisible = false;
+    }
+}
+
+/**
+ * Main convert function that is triggered when we click 'Convert'
+ */
+function convert() {
+    var removeWhitespace = document.getElementById('whitespaceCheckbox').checked;
+    var extractSuperbuy = document.getElementById('superbuyCheckbox').checked;
+    // Convert the links in the text area and place in arrays
+    var links = convertURLs(document.getElementById('content').value.split('\n'), removeWhitespace, extractSuperbuy);
+    validLinks = links.valid;
+    invalidLinks = cleanArray(links.invalid);
+
+    // Set Message for user
+    setHTML('message', getStatus(validLinks.length, invalidLinks.length));
+    if (validLinks.length + invalidLinks.length > 0) {
+        setDisplay('options', false);
+        setDisplay('optionsVisibility', false);
+    }
+
+    // Build valid and invalid links
+    setComponents(true, validLinks);
+    setComponents(false, invalidLinks);
+
+    // Create event handlers for the 'Open Links' buttons
+    document.getElementById('valid-open').addEventListener('click', openLinks);
+    document.getElementById('invalid-open').addEventListener('click', openLinks);
+}
+
+/**
+ * Open a list of links based on the event ID, prompting the user to confirm if there is more than 10
+ */
+function openLinks(e) {
+    if (e.target.id == 'invalid-open') {
+        if (invalidLinks.length <= 10 || invalidLinks.length > 10 && confirm(CONFIRM.replace('{}', invalidLinks.length))) {
+            invalidLinks.forEach(function (link) {
+                window.open(link);
+            });
+        }   
+    } else {
+        if (validLinks.length <= 10 || validLinks.length > 10 && confirm(CONFIRM.replace('{}', validLinks.length))) {
+            validLinks.forEach(function (link) {
+                window.open(link);
+            });
+        }
+    }
+}
+
+/**
+ * Change inner html of a DOM element
+ */
+function setHTML(elementName, html) {
+    document.getElementById(elementName).innerHTML = html;
+}
+
+
+/**
+ * Set display of html of a DOM element
+ */
+function setDisplay(elementName, visible) {
+    document.getElementById(elementName).style.display = (visible ? 'unset' : 'none');
+}
+
+
+/**
+ * Builds the status message at our given states based on the length of the arrays
+ */
+function getStatus(validLength, invalidLength) {
+    if (validLength + invalidLength == 0) {
+        return 'All the links are invalid.';
+    } else {
+        return 'Converted ' + validLength + linkOrLinks(validLength)
+            + ' Found ' + invalidLength + ' invalid' + linkOrLinks(invalidLength);
+    }
+}
+
+
+/**
+ *  English Grammar to find whether we have a link or links
+ */
+function linkOrLinks(count) {
+    return count == 1 ? ' link.' : ' links.';
+}
+
+
+/**
+ * Clean up an array by removing all the empty string
+ */
+function cleanArray(array) {
+    var newArray = [];
+    array.forEach(function (elem) {
+        if (elem != '')
+            newArray.push(elem);
+    });
+    return newArray;
+}
+
+
+/* Prepare components to show valid and invalid links */
+function setComponents(isValid, array) {
+    // Do not process empty array of lists
+    if (array.length == 0)
+        return;
+
+    // Set display of text area to false and show the built links
+    var id = isValid ? 'valid' : 'invalid';
+    setDisplay('content', false);
+    setDisplay(id, true);
+
+    // Build HTML
+    var html = '';
+    array.forEach(function (link) {
+        html += '<a href=\'' + link + '\' target=\'_blank\'>' + link + '</a><br>'
+    });
+
+    // Set components
+    setHTML(id + '-content', html);
+    setHTML(id + '-hidden', html);
+}
+
+
+/**
+ * Reset HTML elements on the web page
+ */
+function reset() {
+    document.getElementById('content').value = '';
+    setDisplay('content', true);
+    if (optionsVisible) {
+        setDisplay('options', true);
+    }
+    setDisplay('optionsVisibility', true);
+    setHTML('message', '');
+    setDisplay('valid', false);
+    setDisplay('invalid', false);
+    setHTML('valid-copy', 'Copy to clipboard');
+    setHTML('invalid-copy', 'Copy to clipboard');
+    // Remove event listeners for the 'open' buttons
+    document.getElementById('valid-open').removeEventListener('click', openLinks);
+    document.getElementById('invalid-open').removeEventListener('click', openLinks);
+}
+
